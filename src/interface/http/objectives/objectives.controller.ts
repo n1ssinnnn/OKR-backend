12c5e817811e40ad612c568cdb2569aa8b68276c
Ohmluvdog <yeanton50@gmail.com>
2026-08-12 19:45:02 +0700

    feat : key-result & objective
    feat : key-results dto
    feat : objectives dto & controller

---
+++ src/interface/http/objectives/objectives.controller.ts
@@ -0,0 +1,36 @@
+import Elysia from "elysia"
+import { t } from "elysia"
+import { CreateObjectiveDTO } from "./objectives.dto"
+import type { CreateObjectiveUseCase } from "../../../application/use-cases/objectives/create-objective.usecase"
+import type { GetObjectivesUseCase } from "../../../application/use-cases/objectives/get-objectives.usecase"
+
+export const createObjectivesController = (
+    createObjective: CreateObjectiveUseCase,
+    getObjectives: GetObjectivesUseCase,
+) =>
+    new Elysia({ prefix: "/objectives" })
+        .get("/", async ({ query, status }) => {
+            try {
+                return await getObjectives.executeByCycle(query.cycleId)
+            } catch (e: any) {
+                return status(400, { message: e.message })
+            }
+        }, {
+            query: t.Object({ cycleId: t.String() }),
+        })
+
+        .get("/:id", async ({ params, status }) => {
+            try {
+                return await getObjectives.executeById(params.id)
+            } catch (e: any) {
+                return status(404, { message: e.message })
+            }
+        })
+
+        .post("/", async ({ body, status }) => {
+            try {
+                return await createObjective.execute(body)
+            } catch (e: any) {
+                return status(400, { message: e.message })
+            }
+        }, { body: CreateObjectiveDTO })
